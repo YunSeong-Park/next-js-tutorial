0b62044a4f43fcba954ba381eca9982bc09dc0f1
YunSeong Park <qkrdbstjd95@naver.com>
2022-05-03 18:34:26 +0900

    [New Feature] next/image 컴포넌트 테스트

--- nextjs-blog/pages/post/first-post.js
+++ nextjs-blog/pages/post/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
@@ -8,7 +9,17 @@ export default function FirstPost() {
         <Link href="/">
           <a>Back to home</a>
         </Link>
+        <YourComponent />
       </h2>
     </>
   );
 }
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
